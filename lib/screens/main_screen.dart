f2101c782231d585cc93b83abc9d9ac14716a16d
FuzZePL <79836488+FuzZePL@users.noreply.github.com>
2021-11-17 10:42:01 +0100

    xd

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,111 @@
+import 'package:first_flutter_app/data/answer.dart';
+import 'package:first_flutter_app/data/questions.dart';
+import 'package:first_flutter_app/widget/answer_que.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  var i = 0;
+  var points = 0;
+  List<Answer> qAndA = Question.questions;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final margin = size.height * 0.02;
+
+    if (i < 4) {
+      return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Container(
+          height: size.height,
+          width: size.width,
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                    top: 8,
+                    bottom: 8,
+                    left: 16,
+                    right: 16,
+                  ),
+                  child: Text(
+                    qAndA[i].question,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 34,
+                    ),
+                  ),
+                ),
+                Image.asset('assets/avatar.png'),
+                SizedBox(
+                  height: margin * 2.5,
+                ),
+                AnswerQue(
+                  onTap: () {
+                    points = points + qAndA[i].points[0];
+                    i += 1;
+                    setState(() {});
+                  },
+                  answer: qAndA[i].answers[0],
+                ),
+                SizedBox(
+                  height: margin,
+                ),
+                AnswerQue(
+                  onTap: () {
+                    points = points + qAndA[i].points[1];
+                    i += 1;
+                    setState(() {});
+                  },
+                  answer: qAndA[i].answers[1],
+                ),
+                SizedBox(
+                  height: margin,
+                ),
+                AnswerQue(
+                  onTap: () {
+                    points = points + qAndA[i].points[2];
+                    i += 1;
+                    setState(() {});
+                  },
+                  answer: qAndA[i].answers[2],
+                ),
+                SizedBox(
+                  height: margin,
+                ),
+                AnswerQue(
+                  onTap: () {
+                    points = points + qAndA[i].points[3];
+                    i += 1;
+                    setState(() {});
+                  },
+                  answer: qAndA[i].answers[3],
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        body: Container(
+          height: size.height,
+          width: size.width,
+          alignment: Alignment.center,
+          child: Text(
+            points.toString(),
+          ),
+        ),
+      );
+    }
+  }
+}
